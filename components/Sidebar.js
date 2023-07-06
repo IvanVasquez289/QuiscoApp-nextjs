25acd35b25945b81498460a5a6003ce61e30f18c
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-06 13:28:00 -0600

    Agregando imagenes

---
+++ components/Sidebar.js
@@ -0,0 +1,10 @@
+import Image from "next/future/image"
+const Sidebar = () => {
+  return (
+    <>
+        <Image src={"/assets/img/logo.svg"} width={300} height={100} alt="imagen logo"/>
+    </>
+  )
+}
+
+export default Sidebar
