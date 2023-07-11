c8eabc1261ee8670a35cdbeef6d8f4325e54d9f2
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-11 02:09:46 -0600

    Llevar al menu principal

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 import {toast} from 'react-toastify'
+import { useRouter } from "next/router";
 const QuioscoContext = createContext()
 
 const QuioscoProvider = ({children}) => {
@@ -10,6 +11,8 @@ const QuioscoProvider = ({children}) => {
     const [producto, setProducto] = useState({})
     const [modal, setModal] = useState(false)
     const [pedido, setPedido] = useState([])
+
+    const router = useRouter()
 
     const obtenerCategorias = async () => {
         const {data} = await axios('/api/categoria')
@@ -28,6 +31,7 @@ const QuioscoProvider = ({children}) => {
     const handleClickCategoria = id => {
         const categoria = categorias.filter(cat => cat.id === id)
         setCategoriaActual(categoria[0])
+        router.push('/')
     }
     
     const handleSetProducto = (producto) => {
