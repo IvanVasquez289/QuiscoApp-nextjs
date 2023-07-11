8e7319b1f089ecf29574d79ffc46995a7cf565ee
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-10 21:51:43 -0600

    Detectando la Url para recordar el progreso

--- components/Pasos.js
+++ components/Pasos.js
@@ -6,21 +6,51 @@ const pasos = [
 ]
 const Pasos = () => {
   const router = useRouter()
+
+  const calcularProgreso = () => {
+    let valor;
+    switch (router.pathname) {
+        case '/':
+            valor = '5%'
+            break;
+        case '/resumen':
+            valor = '50%'
+            break;
+        case '/total':
+            valor = '100%'
+            break;
+    
+        default:
+            break;
+    }
+    return valor
+  }
+  
   return (
     <>
-    <div className="flex justify-between mb-5">
-        {pasos.map(paso => (
-            <button 
-                key={paso.paso}
-                className="font-bold text-2xl"
-                onClick={()=>{
-                    router.push(paso.url)
-                }}
+        <div className="flex justify-between mb-5">
+            {pasos.map(paso => (
+                <button 
+                    key={paso.paso}
+                    className="font-bold text-2xl"
+                    onClick={()=>{
+                        router.push(paso.url)
+                    }}
+                >
+                    {paso.nombre}
+                </button>
+            ))}
+        </div>
+
+        <div className=" bg-gray-100 mb-6">
+            <div 
+                className="rounded-full bg-amber-500 h-2 leading-none text-center "
+                style={
+                    {width: calcularProgreso() }
+                }
             >
-                {paso.nombre}
-            </button>
-        ))}
-    </div>
+            </div>
+        </div>
     </>
   )
 }
