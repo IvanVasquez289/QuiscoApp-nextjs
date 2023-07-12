8f35fab381d9250db9176fa7b9d3b95cd3585738
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-11 19:38:34 -0600

    Agregando registros a la DB

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,6 +1,16 @@
+import { PrismaClient } from "@prisma/client"
 export default async function handler(req, res) {
+    const prisma = new PrismaClient()
     if(req.method === 'POST'){
-        console.log(req.body)
-        res.status(200).json({hola:'este es un metodo post'})
+        // console.log(req.body)
+        const orden = await prisma.orden.create({
+            data:{
+                nombre: req.body.nombre,
+                fecha: req.body.fecha,
+                total: req.body.total,
+                pedido: req.body.pedido
+            }
+        })
+        res.status(200).json(orden)
     }
 }
