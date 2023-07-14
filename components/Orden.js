c1340b8fb1c24dd35c960861c392a562a028d484
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-14 14:30:36 -0600

    Mostrando ordenes

---
+++ components/Orden.js
@@ -0,0 +1,39 @@
+import Image from "next/future/image";
+import formatearDinero from "../helpers";
+const Orden = ({ orden }) => {
+  const { nombre, total, pedido, id } = orden;
+  return (
+    <div className="border p-10 space-y-5">
+      <h3 className=" text-2xl font-bold">Orden: {id}</h3>
+      <p className="text-lg font-bold">Cliente: {nombre}</p>
+
+      <div>
+        {pedido.map((platillo) => (
+          <div
+            key={platillo.id}
+            className="md:flex items-center py-3 border-b last-of-type:border-b-0"
+          >
+            <div className="md:w-32">
+              <Image
+                src={`/assets/img/${platillo.imagen}.jpg`}
+                alt={`imgaen platillo ${platillo.nombre}`}
+                width={300}
+                height={400}
+              />
+            </div>
+            <div className="p-5 space-y-2">
+              <p className=" text-amber-500 text-xl font-bold">{platillo.nombre}</p>
+              <p className="text-lg font-bold">Cantidad:{platillo.cantidad}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="md:flex items-center my-10">
+            <p className=" text-3xl font-black text-amber-500">Total a pagar: {formatearDinero(total)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Orden;
--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,7 @@
 import useSWR from 'swr'
 import axios from 'axios';
 import AdminLayout from "../layout/AdminLayout";
-
+import Orden from '../components/Orden';
 export default function Admin(){
     const fetcher = () => axios('/api/ordenes').then(datos => datos.data)
     const { data, error, isLoading } = useSWR('/api/ordenes', fetcher)
@@ -14,6 +14,13 @@ export default function Admin(){
         >
             <h1 className=" text-4xl font-black">Panel de Administracion</h1>
             <p className="text-2xl my-10">Administra las ordenes</p>
+
+            {data && data.length ? data.map( orden => (
+                <Orden
+                    key={orden.id}
+                    orden={orden}
+                />
+            ) ) : 'No hay'}
         </AdminLayout>
     )
 }
